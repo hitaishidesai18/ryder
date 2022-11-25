4cf2336519689ea42e8fb36c6f0ac9cf69eaa3db
VaishnaviShri <vaishnavishri2019@gmail.com>
2022-11-26 00:38:08 +0530

    Setup dependencies for firebase authentication

--- app/src/main/java/com/example/ryder/ui/main/LoginViewModel.java
+++ app/src/main/java/com/example/ryder/ui/main/LoginViewModel.java
@@ -1,7 +1,19 @@
 package com.example.ryder.ui.main;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import androidx.lifecycle.ViewModel;
 
 public class LoginViewModel extends ViewModel {
     // TODO: Implement the ViewModel
+    private FirebaseAuth mAuth;
+
+    public boolean isUserSignedIn() {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            return true;
+        }
+        return false;
+    }
 }
